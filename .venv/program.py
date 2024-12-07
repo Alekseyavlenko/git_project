98c1c0032aaeda4ce7a4b85b4dcb7de9718fdcd3
alkseypavlenko <aleksejpavlenko@gmail.com>
2024-12-07 16:48:45 +0300

    $ git config
    Signed-off-by: alkseypavlenko <aleksejpavlenko@gmail.com>

--- .venv/program.py
+++ .venv/program.py
@@ -1,7 +1,7 @@
 def main():
     print('My first git progra')
     print('And I change it eery da')
-# https://github.com/
+# https://github.com/Alekseyavlenko/git_project
 
 if __name__ == '__main__':
     main()
