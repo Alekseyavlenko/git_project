4fd5a84667d7e1d9f4a13996e8ee4a95c12a5556
alkseypavlenko <aleksejpavlenko@gmail.com>
2024-12-07 16:36:13 +0300

    $ git config --global user.name "Alekseyavlenko" $ git config --global user.email "aleksejpavlenko1665@gmail.com"
    Signed-off-by: alkseypavlenko <aleksejpavlenko@gmail.com>

--- .venv/program.py
+++ .venv/program.py
@@ -1,7 +1,7 @@
 def main():
-    print('My first git program')
+    print('My first git progra')
     print('And I change it every da')
-
+# https://github.com/
 
 if __name__ == '__main__':
     main()
